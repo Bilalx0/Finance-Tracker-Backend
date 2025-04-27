ad07cec090a3240283b21289ef92d14ff84bcc81
Muhammad Bilal <muhammadbilaal098@gmail.com>
2025-04-27 18:30:42 +0500

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const targetRoutes = require('./routes/targetRoutes')
 
 const app = express();
 app.use(cors({
-  origin: 'https://finance-tracker-version-2.vercel.app',
+  origin: ['https://finance-tracker-version-2.vercel.app', 'http://localhost:5173'],
   methods: 'POST, GET, OPTIONS',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true 
